fec5ee35348ab38cc4f834730aa8a40d4ea6ef69
xj962458 <2291921855@qq.com>
2020-05-14 11:01:10 +0800

    xj

--- test/test.c
+++ test/test.c
@@ -1,36 +1,13 @@
-#include <stdio.h>
-#include<stdlib.h>
+#include<stdio.h>  
+#include<stdlib.h> 
 #include<string.h>
-typedef struct student{
-    char a[3][40];
-    int no[3];
-    struct student *next;
-}STU;
-int main()
-{
-    int i,j,k;
-    char b[3][40];
-    STU *p=(STU*)malloc(sizeof(STU));
-    for(i=0;i<3;i++)
-    {
-        printf("请输入姓名:");
-        scanf("%s",(p->a)+i);
-        printf("请输入年龄:");
-        scanf("%d",(p->no)+i);
-    }
-    printf("\n\n信息如下:\n");
-    printf("姓名\t年龄\n");
-    for(j=0;j<3;j++)
-    {
-        printf("%s\t",*((p->a)+j));
-        printf("%d",*((p->no)+j));
-        printf("\n");
-    }
-    for(k=0;k<3;k++)
-        strcpy(b[k],*((p->a)+k));
-    for(j=0;j<3;j++)
-    {
-        printf("%s\t",b[j]);
-        printf("\n");
-    }
-}
+typedef struct student{char name[40];char no[18];int xb;int age;char tele[20];struct student *next;}STU;
+void ListCreate(STU *L, int n);void LIstSearch(STU *L);void ListModify(STU *L);void ListInsert(STU *L);void ListDelete(STU *L);void Input(STU *p,int i);void Menu();                        
+int main(){int item,n;STU *L=NULL;L=(STU *)malloc(sizeof(STU));L->next=NULL;do{Menu();printf("请输入相应的数字，进行相应的操作:\n");scanf("%d",&item);system("cls");switch(item){case 1:printf("请输入您要录入的考生人数:");scanf("%d",&n);ListCreate(L,n);getchar();printf("\n请按任意键返回主菜单\n");getchar();system("cls");break;case 2:LIstSearch(L);getchar();printf("\n请按任意键返回主菜单\n");getchar();system("cls");break;case 3:ListModify(L);printf("\n请按任意键返回主菜单\n");getchar();system("cls");break;case 4:ListDelete(L);getchar();printf("\n请按任意键返回主菜单\n");getchar();system("cls");break;case 5:ListInsert(L);getchar();printf("\n请按任意键返回主菜单\n");getchar();system("cls");break;case 0:printf("即将退出考生信息管理系统.....");exit(0);default:printf("您输入的指令不正确，请重新输入");}printf("\n\n");}while(item);return 0;}
+void ListCreate(STU *L, int n){int i; for (i = 0; i<n; i++){STU *p;p=NULL;p = (STU *)malloc(sizeof(STU));Input(p,i);p->next = L->next;L->next = p;}}
+void LIstSearch(STU *L){char n[40];STU *p=L->next;if (p == NULL)printf("数据为空，无法查找！");else{printf("请输入您要查找的考生姓名:");scanf("%s",n);while (strcmp(p->name, n) != 0){p = p->next;if (p == NULL){printf("没有找到相关信息\n");return;}}printf("该考生的相关信息如下:\n");printf("姓名\t身份证号\t\t性别\t年龄\t电话号码\n");printf("%s\t",p->name);printf("%s",p->no);printf("\t%d",p->xb);printf("\t%d",p->age);printf("\t%s",p->tele);printf("\n");}}
+void ListModify(STU *L){int x,a,item;char nam[40],no[18],tel[20],n[40];STU *p=L->next;if (p == NULL)printf("数据为空，无法修改！");else{printf("请输入您要修改的考生姓名:");scanf("%s",n);while(strcmp(p->name, n) != 0){p = p->next;if (p == NULL){printf("没有找到相关信息\n");return;}}printf("请选择您要修改的考生信息类型:\n");printf("1:姓名\t 2:身份证号\t 3:性别\t 4:年龄\t 5:电话号码\n");scanf("%d",&item);switch(item){case 1:printf("请输入您修改后的姓名:");scanf("%s",nam);strcpy(p->name,nam);break;case 2:printf("请输入您修改后的身份证号:");scanf("%s",no);strcpy(p->no,no);break;case 3:printf("请输入您修改后的性别:");scanf("%d",&x);p->xb=x;break;case 4:printf("请输入您修改后的年龄:");scanf("%d",&a);p->age=a;break;case 5:printf("请输入您修改后的电话号码:");scanf("%s",tel);strcpy(p->tele,tel);break;default:printf("您输入的指令不正确!\n");}printf("修改成功!");}}
+void ListDelete(STU *L){char n[40];STU *p=L->next,*pre=L;if(p==NULL)printf("数据为空，无法删除！");else{printf("请输入您要删除的考生姓名:");scanf("%s",n);while(strcmp(p->name,n)!=0){    pre=p;    p=pre->next;if(p==NULL){printf("没有找到相关信息，无法删除\n");return;}}pre->next=p->next;free(p);printf("删除成功");}}
+void ListInsert(STU *L){STU *s = NULL;s = (STU *)malloc(sizeof(STU));printf("请输入您要插入的考生的姓名:");scanf("%s",s->name);printf("请输入您要插入的考生的身份证号:");scanf("%s",s->no);printf("请输入您要插入的考生的性别（女性为0，男性为1）:");scanf("%d",&s->xb);printf("请输入您要插入的考生的年龄:");scanf("%d",&s->age);printf("请输入您要插入的考生的电话号码:");scanf("%s",s->tele);s->next = L->next;L->next = s;}
+void Input(STU *p,int i){printf("请输入第%d名考生的姓名:",i+1);scanf("%s",p->name);printf("请输入第%d名考生的身份证号:",i+1);scanf("%s",p->no);printf("请输入第%d名考生的性别（女性为0，男性为1）:",i+1);scanf("%d",&p->xb);printf("请输入第%d名考生的年龄:",i+1);scanf("%d",&p->age);printf("请输入第%d名考生的电话号码:",i+1);scanf("%s",p->tele);}
+void Menu(){printf("\n\n");printf("\t\t\t===================考试报名管理系统==================\n");printf("\t\t\t* 作者:武新纪  班级:人工智能191  学号:3190707121    *\n");printf("\t\t\t*                                                   *\n");printf("\t\t\t*          1>. 录入考生信息                         *\n");printf("\t\t\t*          2>. 查找考生信息                         *\n");printf("\t\t\t*          3>. 修改考生信息                         *\n");printf("\t\t\t*          4>. 删除考生信息                         *\n");printf("\t\t\t*          5>. 插入考生信息                         *\n");printf("\t\t\t*          0>. 退出管理系统                         *\n");printf("\t\t\t*                                    欢迎使用本系统!*\n");printf("\t\t\t=====================================================\n");printf("\t\t\t输入选项，按回车进入选项:                             \n");}
--- 系统/商品库存管理系统/商品库存管理系统.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-struct node
-{
-    char no[12];        //商品编号
-    char name[40];      //名称
-    int workload;       //库存量
-    struct node *next;  //指向下一节点的指针
-};
