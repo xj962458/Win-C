fc9a684b0060603f4a3a1fc860ff4a38fbbcc836
xj962458 <2291921855@qq.com>
2021-03-11 13:25:50 +0800

    xj-20210311

--- 系统/通讯录管理系统/简易通讯录.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-struct node
-{
-    char name[40];          //姓名
-    char tele[20];          //电话
-    char Email[40];         //电子邮箱
-    char addr[80];          //通讯地址
-    struct node *next;      //指向下一节点的指针
-};
